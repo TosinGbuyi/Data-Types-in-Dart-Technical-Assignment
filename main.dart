41081322b9a79414db9124f096d581dcae278ebe
Tosin Gbuyi <tosingbuyi@gmail.com>
2024-03-07 09:39:13 +0100

    first commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+viod main() {
+int i = 3;
+switch (i) {
+    case 1:
+    print("The value is 1");
+    break;
+    case 2:
+    print("The value is 2");
+    case 3:
+    print("The value is 3");
+    break;
+    default:
+    Print (the value is out of range )
+}
+
+}
